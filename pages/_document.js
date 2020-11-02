72db538d21939f1850bfc64af15b72ae9ba0b87f
Stefan Buck <github@stefanbuck.com>
2020-11-02 20:47:48 +0100

    Add plausible

---
+++ pages/_document.js
@@ -0,0 +1,28 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+const PLAUSIBLE_ENABLED = process.env.PLAUSIBLE_ENABLED || false
+
+const plausibleSnippet = 'function() { (window.plausible.q = window.plausible.q || []).push(arguments) }';
+const plausibleFunction = PLAUSIBLE_ENABLED ? plausibleSnippet : 'function() {}'
+
+export default class NextSite extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          {PLAUSIBLE_ENABLED && (<script async defer data-domain="tentacle.app" src="https://plausible.io/js/plausible.js" />)}
+          <script
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: `window.plausible = window.plausible || ${plausibleFunction}`
+            }}
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
