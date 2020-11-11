c3a7df74ee7221c39584533ad56b0c76b8d348f4
Stefan Buck <github@stefanbuck.com>
2020-11-12 00:42:02 +0100

    Update fixtures to include issues

--- pages/api/fixtures/update-fixtures.sh
+++ pages/api/fixtures/update-fixtures.sh
@@ -5,3 +5,4 @@ curl http://localhost:3000/api/github/facebook/react > pages/api/fixtures/facebo
 curl http://localhost:3000/api/github/facebook/jest > pages/api/fixtures/facebook_jest.json
 curl http://localhost:3000/api/github/vercel/next.js > pages/api/fixtures/vercel_next.js.json
 curl http://localhost:3000/api/github/backstage/backstage > pages/api/fixtures/backstage_backstage.json
+curl http://localhost:3000/api/github/tdeekens/flopflip > pages/api/fixtures/tdeekens_flopflip.json
