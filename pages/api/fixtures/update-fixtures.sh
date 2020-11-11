76adf479d63ce1dd1983d4c10a4c666ce38b8e54
Stefan Buck <github@stefanbuck.com>
2020-11-11 22:50:45 +0100

    Update fixtures

--- pages/api/fixtures/update-fixtures.sh
+++ pages/api/fixtures/update-fixtures.sh
@@ -4,4 +4,4 @@ set -e
 curl http://localhost:3000/api/github/facebook/react > pages/api/fixtures/facebook_react.json
 curl http://localhost:3000/api/github/facebook/jest > pages/api/fixtures/facebook_jest.json
 curl http://localhost:3000/api/github/vercel/next.js > pages/api/fixtures/vercel_next.js.json
-curl http://localhost:3000/api/github/spotify/backstage > pages/api/fixtures/spotify_backstage.json
+curl http://localhost:3000/api/github/backstage/backstage > pages/api/fixtures/backstage_backstage.json
