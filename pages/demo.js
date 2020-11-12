8ab84dab7fddaae227805d72cec5f7eda4bab2bb
Stefan Buck <github@stefanbuck.com>
2020-11-12 01:14:11 +0100

    Add demo route

--- pages/demo.js
+++ pages/demo.js
@@ -1,18 +1,15 @@
 import { useState } from 'react'
-import { useRouter } from 'next/router'
 import Layout from '../components/layout'
 import List from '../components/list'
 import Filters from '../components/filters'
 
 export default function IndexPage() {
-  const router = useRouter()
-  const [filter, setFilter] = useState('is:merged');
+  const [filter, setFilter] = useState('is:merged repo:backstage/backstage');
 
-  const slug = router.query.slug?.join('/') || ''
   return (
     <Layout>
       <Filters value={filter} setValue={setFilter} />
-      <List slug={slug} filter={filter} />
+      <List filter={filter} />
     </Layout>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
-import { useState } from 'react'
 import Layout from '../components/layout'
-import List from '../components/list'
-import Filters from '../components/filters'
 
 export default function IndexPage() {
-  const [filter, setFilter] = useState('is:merged repo:backstage/backstage');
-
   return (
-    <Layout>
-      <Filters value={filter} setValue={setFilter} />
-      <List filter={filter} />
-    </Layout>
+    <Layout></Layout>
   )
 }
