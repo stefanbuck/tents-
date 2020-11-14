81f22bcae302b02fd5b028d78b852a32de95949b
Stefan Buck <github@stefanbuck.com>
2020-11-15 00:38:23 +0100

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react', 'plugin:react/recommended'],
+  parserOptions: {
+    es2021: true,
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        tabWidth: 2,
+      },
+    ],
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/jsx-filename-extension': 0,
+  },
+};
