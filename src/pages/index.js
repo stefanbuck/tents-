ed9c8151e354e35071d6b79ec2e0b07c5b004a7b
Stefan Buck <github@stefanbuck.com>
2020-11-23 22:48:35 +0100

    Remove demo route

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
+import { useState } from 'react';
 import Layout from '@/layouts/default';
+import List from '@/components/list';
+import Filters from '@/components/filters';
 
 export default function IndexPage() {
-  return <Layout />;
+  const [filter, setFilter] = useState('is:merged repo:backstage/backstage');
+
+  return (
+    <Layout>
+      <Filters value={filter} setValue={setFilter} />
+      <List filter={filter} />
+    </Layout>
+  );
 }
