a49f44ad6b3b2b33086c831b614edf370cbfc2fe
Stefan Buck <github@stefanbuck.com>
2020-11-23 22:39:20 +0100

    Update backstage fixture

--- src/pages/api/fixtures/update-fixtures.sh
+++ src/pages/api/fixtures/update-fixtures.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-curl http://localhost:3000/api/github/facebook/react > pages/api/fixtures/facebook_react.json
-curl http://localhost:3000/api/github/facebook/jest > pages/api/fixtures/facebook_jest.json
-curl http://localhost:3000/api/github/vercel/next.js > pages/api/fixtures/vercel_next.js.json
-curl http://localhost:3000/api/github/backstage/backstage > pages/api/fixtures/backstage_backstage.json
-curl http://localhost:3000/api/github/tdeekens/flopflip > pages/api/fixtures/tdeekens_flopflip.json
+curl http://localhost:3000/api/github/facebook/react > src/pages/api/fixtures/facebook_react.json
+curl http://localhost:3000/api/github/facebook/jest > src/pages/api/fixtures/facebook_jest.json
+curl http://localhost:3000/api/github/vercel/next.js > src/pages/api/fixtures/vercel_next.js.json
+curl http://localhost:3000/api/github/backstage/backstage > src/pages/api/fixtures/backstage_backstage.json
+curl http://localhost:3000/api/github/tdeekens/flopflip > src/pages/api/fixtures/tdeekens_flopflip.json
