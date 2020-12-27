7665d0b49848d1cc9508041593d768ea788d2779
Stefan Buck <github@stefanbuck.com>
2020-12-28 00:32:46 +0100

    Add hero component used to pitch the project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,19 @@
 import { useState } from 'react';
+import { useSession } from 'next-auth/client';
 import Layout from '@/layouts/default';
 import List from '@/components/list';
 import Filters from '@/components/filters';
+import Hero from '@/components/hero';
 
 export default function IndexPage() {
   const [filter, setFilter] = useState('is:merged repo:backstage/backstage');
+  const [session, loading] = useSession();
+
+  if (loading) return null;
 
   return (
     <Layout>
+      {!session && <Hero />}
       <Filters value={filter} setValue={setFilter} />
       <List filter={filter} />
     </Layout>
