6a8600086413f74bfd5b4db38bb82cf47c2f4b9a
Stefan Buck <github@stefanbuck.com>
2021-02-20 00:35:38 +0100

    Show tips below filter input

--- src/components/filters.js
+++ src/components/filters.js
@@ -1,6 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 
+function Tips() {
+  const tips = [
+    'Follow long discussions with `comments:>50`.',
+    '`no:milestone` will show everything without a milestone.',
+    'Find everything you created by `author:octocat`.',
+    'Adding `no:label` will show everything without a label.',
+    'Ears burning? Get @octocat mentions with `mentions:octocat`.',
+    'Exclude your own issues with `-author:octocat`.',
+    "Find all pull requests that aren't related to any open issues with `-linked:issue`.",
+    'Filter pull requests by the default branch with `base:main`.',
+    'Exclude everything labeled bug with `-label:bug`.',
+    'Add `no:assignee` to see everything that’s not assigned.',
+  ];
+
+  const [index, setIndex] = useState(0);
+  function showNext() {
+    if (index + 1 >= tips.length) {
+      setIndex(0);
+    } else {
+      setIndex(index + 1);
+    }
+  }
+
+  return (
+    <div className="mt-1 mb-4 ml-1 text-xs text-gray-500">
+      <button
+        type="button"
+        className="text-blue-600"
+        onClick={() => showNext()}
+      >
+        Show another tip:
+      </button>{' '}
+      {tips[index]}
+    </div>
+  );
+}
+
 export default function Filters({ onChange }) {
   const router = useRouter();
 
@@ -37,16 +74,15 @@ export default function Filters({ onChange }) {
 
   return (
     <div className="p-2 my-6 rounded bg-blue-gray-200">
-      <div className="flex justify-between">
-        Filters
-        <input
-          onChange={(event) => setFilterString(event.target.value)}
-          className="w-full px-1 ml-2 bg-white"
-          value={filter}
-        />
-      </div>
+      <input
+        onChange={(event) => setFilterString(event.target.value)}
+        className="w-full px-2 py-1 bg-white"
+        value={filter}
+      />
+
+      <Tips />
       <details className="text-sm">
-        <summary className="italic underline">Examples</summary>
+        <summary className="italic underline cursor-pointer">Examples</summary>
         <ul className="truncate list-disc list-inside">
           {examples.map((example, index) => (
             // eslint-disable-next-line react/no-array-index-key
