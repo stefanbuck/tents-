59c09ddf95d03efa735752daa637b521e7e720b3
Stefan Buck <github@stefanbuck.com>
2021-07-18 21:50:19 +0200

    Update hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -2,21 +2,33 @@ import Link from 'next/link';
 
 export default function LandingContent() {
   return (
-    <div className="p-6 my-4 rounded bg-blue-gray-200">
+    <div
+      className="p-6 mb-4 mt-6 bg-white border rounded border-blue-gray-200 bg-no-repeat bg-right-bottom"
+      style={{
+        backgroundImage: 'url(./tentacle.png)',
+        backgroundPosition: 'right 85%',
+      }}
+    >
       <div>
         <h1 className="text-3xl font-semibold">Rethink how we use GitHub</h1>
-        <p className="pt-6">
+        <p className="pt-6 text-gray-500">
           Tentacle is a prototype to explore the idea of consuming GitHub issues
           and pull requests in a Twitter-like way.
         </p>
-        <p className="pt-6">
-          If you want to follow the evolution of a project, want to see what
-          people are talking about, what is happening, and what is coming next,
-          Tentacle is for you.{' '}
-          <Link href="/about">
-            <span className="underline cursor-pointer">Read on</span>
-          </Link>
+        <p className="pt-6 pb-4 text-gray-500">
+          <ul className="pl-6">
+            <li className="list-disc">Follow the evolution of a project</li>
+            <li className="list-disc">See what people are talking about </li>
+            <li className="list-disc">
+              Stay up to date with the latest changes
+            </li>
+          </ul>
         </p>
+        <Link href="/about">
+          <span className="text-blue-600 font-medium cursor-pointer">
+            See what else it can do!
+          </span>
+        </Link>
       </div>
     </div>
   );
