fb32cc9b28aa8ad3a90bf445a463deeb43e74c06
Stefan Buck <github@stefanbuck.com>
2021-07-18 21:23:17 +0200

    Add about page to main nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -2,7 +2,8 @@ import Link from 'next/link';
 
 export default function Nav() {
   return (
-    <div className="flex items-center justify-center text-white justify-items-center">
+    <div className="flex items-center gap-6 justify-center text-white justify-items-center">
+      <Link href="/about">About</Link>
       <Link href="/login">Sign in</Link>
     </div>
   );
